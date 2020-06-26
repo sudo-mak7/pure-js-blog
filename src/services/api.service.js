41338cf2384286cbe178c48de181e4d3a6ca75a9
mak7 <zacevmax@andex.ru>
2020-06-26 12:45:19 +0400

    Added firebase connection and DB interacion

---
+++ src/services/api.service.js
@@ -0,0 +1,21 @@
+class ApiService {
+  constructor(baseUrl) {
+    this.url = baseUrl
+  }
+
+  async createPost(post) {
+    try {
+      const request = new Request(this.url + '/posts.json', {
+        method: 'post',
+        body: JSON.stringify(post)
+      })
+      const response = await fetch(request)
+      return await response.json()
+    } catch (error) {
+      console.error(error)
+      
+    }
+  }
+}
+
+export const apiService = new ApiService('https://pure-js-blog-b4fd5.firebaseio.com')
