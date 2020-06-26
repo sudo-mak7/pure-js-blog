1bc82696ad28fbcf4c9f44eb47f1d142f22cdff4
mak7 <zacevmax@andex.ru>
2020-06-26 13:13:00 +0400

    Created service to get data from firebase

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -9,13 +9,28 @@ class ApiService {
         method: 'post',
         body: JSON.stringify(post)
       })
-      const response = await fetch(request)
-      return await response.json()
+      return useRequest(request)
     } catch (error) {
       console.error(error)
       
     }
   }
+
+  async fetchPosts() {
+    try {
+      const request = new Request(`${this.url}/posts.json`, {
+        method: 'get'
+      })
+      return useRequest(request)
+    } catch (error) {
+      console.error()
+    }
+  }
+}
+
+async function useRequest(request) {
+  const response = await fetch(request)
+  return await response.json()
 }
 
 export const apiService = new ApiService('https://pure-js-blog-b4fd5.firebaseio.com')
