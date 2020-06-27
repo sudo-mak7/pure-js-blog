e00eeb3e28098b04414f37e49217d673f54751c1
mak7 <zacevmax@andex.ru>
2020-06-27 18:22:23 +0400

    Created button which provides add post to favorites and delete from it

--- src/components/posts.component.js
+++ src/components/posts.component.js
@@ -8,6 +8,10 @@ export class PostsComponent extends Component {
     this.loader = loader
   }
 
+  init() {
+    this.$el.addEventListener('click', buttonHandler.bind(this))
+  }
+
   async onShow() {
     this.loader.show()
 
@@ -30,7 +34,9 @@ function renderPost(post) {
     ? '<li class="tag tag-blue tag-rounded">Новость</li>'
     : '<li class="tag tag-rounded">Заметка</li>'
 
-  const button = '<button class="button-round button-small button-primary">Сохранить</button>'
+  const button = (JSON.parse(localStorage.getItem('favorites')) || []).includes(post.id)
+    ? `<button class="button-round button-small button-danger" data-id="${post.id}">Удалить из избранного</button>`
+    : `<button class="button-round button-small button-primary" data-id="${post.id}">В избранное</button>`
 
   return `
     <div class="panel">
@@ -49,4 +55,26 @@ function renderPost(post) {
     </div>
   </div>
   `
+}
+
+function buttonHandler(event) {
+  const $el = event.target
+  const id = $el.dataset.id
+  if (id) {
+    let favorites = JSON.parse(localStorage.getItem('favorites')) || []
+    
+    if (favorites.includes(id)) {
+      $el.textContent = 'В избранное'
+      $el.classList.add('button-primary')
+      $el.classList.remove('button-danger')
+      favorites = favorites.filter(fId => fId !== id)
+    } else {
+      $el.textContent = 'Удалить из избранного'
+      $el.classList.remove('button-primary')
+      $el.classList.add('button-danger')
+      favorites.push(id)
+    }
+
+    localStorage.setItem('favorites', JSON.stringify(favorites))
+  }
 }
