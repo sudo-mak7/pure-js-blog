fcfcebbf960358fa6a096b768f44d1e518ffe5ba
mak7 <zacevmax@andex.ru>
2020-06-27 19:11:57 +0400

    Added output list of favorites posts

--- src/components/favorite.component.js
+++ src/components/favorite.component.js
@@ -1,7 +1,51 @@
 import { Component } from '../core/component'
+import { apiService } from '../services/api.service'
+import { renderPost } from '../templates/post.template'
 
 export class FavoriteComponent extends Component {
-  constructor(id) {
+  constructor(id, { loader }) {
     super(id)
+    this.loader = loader
   }
+
+  init() {
+    this.$el.addEventListener('click', linkClickHandler.bind(this))
+  }
+
+  onShow() {
+    const favorites = JSON.parse(localStorage.getItem('favorites'))
+    const html = renderList(favorites)
+    this.$el.insertAdjacentHTML('afterbegin', html)
+  }
+
+  onHide() {
+    this.$el.innerHTML = ''
+  }
+}
+
+async function linkClickHandler(event) {
+  event.preventDefault()
+
+  if (event.target.classList.contains('js-link')) {
+    const postId = event.target.textContent
+    this.$el.innerHTML = ''
+
+    this.loader.show()
+
+    const post = await apiService.fetchPostsById(postId)
+    this.loader.hide()
+    this.$el.insertAdjacentHTML('afterbegin', renderPost(post, {withButton: false}))
+  }
+}
+
+function renderList(list = []) {
+  if (list.length) {
+    return `
+      <ul>
+        ${list.map(i => `<li><a href="#" class="js-link">${i}</a></li>`).join(' ')}
+      </ul>
+    `
+  }
+
+  return `<p class="center">Вы пока ничего не добавили</p>`
 }
--- src/services/api.service.js
+++ src/services/api.service.js
@@ -26,7 +26,19 @@ class ApiService {
       console.error()
     }
   }
+
+  async fetchPostsById(id) {
+    try {
+      const request = new Request(`${this.url}/posts/${id}.json`, {
+        method: 'get'
+      })
+      return useRequest(request)
+    } catch (error) {
+      console.error()
+    }
+  }
 }
+
 
 async function useRequest(request) {
   const response = await fetch(request)
